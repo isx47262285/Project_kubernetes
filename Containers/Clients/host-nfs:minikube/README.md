e7ee13c14555f177518ae07029ab49cba3da2b4a
roberto <roberto@localhost.localdomain>
2020-06-07 20:03:29 +0200

    test

---
+++ Containers/Clients/host-nfs:minikube/README.md
@@ -0,0 +1,3 @@
+## ejecucion
+ 
+docker run --rm --name host-nfs -h host-nfs --privileged -it robert72004/host-nfs:minikube 
--- Containers/Clients/host-nfs:minikube/install.sh
+++ Containers/Clients/host-nfs:minikube/install.sh
@@ -16,7 +16,7 @@ useradd -g users -G kusers user03
 echo "local01" | passwd --stdin local01
 echo "local02" | passwd --stdin local02
 echo "local03" | passwd --stdin local03
-echo "192.168.99.102 kserver.edt.org samba-homes ldap nfs-homes" >> /etc/hosts 
+echo "192.168.99.102 kserver.edt.org samba-homes ldap nfs-server" >> /etc/hosts 
 cp /opt/docker/krb5.conf /etc/krb5.conf
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
